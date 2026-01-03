b606046e705d85e5fe2bf5505e463900cd3a1971
Christopher <ckingmaster2@gmail.com>
2026-01-03 16:29:28 +0100

    feat: add UserNotFoundException for missing users

---
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/exceptions/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t01.userapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException() {
+        super("User not found");
+    }
+}
+
