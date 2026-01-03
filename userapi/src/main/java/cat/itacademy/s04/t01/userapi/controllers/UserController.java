d46d280894c051e8380742f48085a2cef6cd17e5
Christopher <ckingmaster2@gmail.com>
2026-01-03 16:50:07 +0100

    feat: add optional name filter to users endpoint

--- userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -16,8 +16,20 @@ public class UserController {
 
 
     @GetMapping
-    public List<User> getAllUsers() {
-        return users;
+    public List<User> getAllUsers(
+        @RequestParam(required = false) String name
+    ) {
+        if (name == null) {
+            return users;
+        }
+
+
+        return users.stream()
+                .filter(user -> user.getName().toLowerCase()
+                .contains(name.toLowerCase())
+                )
+                .toList();
+
     }
 
     @PostMapping
