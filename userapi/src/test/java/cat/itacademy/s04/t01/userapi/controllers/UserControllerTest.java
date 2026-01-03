4226b739c9d24e0b33d74a4d8f5c2537358a95fd
Christopher <ckingmaster2@gmail.com>
2026-01-03 18:05:24 +0100

    test: convert controller tests to integration tests

--- userapi/src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
+++ userapi/src/test/java/cat/itacademy/s04/t01/userapi/controllers/UserControllerTest.java
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
     @Autowired
