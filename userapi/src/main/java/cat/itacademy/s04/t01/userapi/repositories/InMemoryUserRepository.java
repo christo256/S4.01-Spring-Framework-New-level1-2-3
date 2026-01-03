2edaea4f16675a62b683b7ad88894095ecab4787
Christopher <ckingmaster2@gmail.com>
2026-01-03 18:25:05 +0100

    feat: add user repository interface and in-memory implementation

---
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/repositories/InMemoryUserRepository.java
@@ -0,0 +1,47 @@
+package cat.itacademy.s04.t01.userapi.repositories;
+
+import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.*;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository {
+
+    private final List<User> users = new ArrayList<>();
+
+    @Override
+    public User save(User user) {
+        users.add(user);
+        return user;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return users;
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return users.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+
+    }
+
+    @Override
+    public List<User> searchByName(String name) {
+        return users.stream()
+                .filter(user -> user.getName().toLowerCase().contains(name.toLowerCase()))
+                .toList();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return users.stream()
+                .anyMatch(user -> user.getEmail().equalsIgnoreCase(email));
+    }
+}
---
+++ userapi/src/main/java/cat/itacademy/s04/t01/userapi/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package cat.itacademy.s04.t01.userapi.repositories;
+
+import cat.itacademy.s04.t01.userapi.model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+
+    User save(User user);
+
+    List<User> findAll();
+
+    Optional<User> findById(UUID id);
+
+    List<User> searchByName(String name);
+
+    boolean existsByEmail(String email);
+}
